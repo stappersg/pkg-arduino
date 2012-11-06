e224afb3c8742112ae176013e3f57649abc02815
Scott Howard <showard314@gmail.com>
2012-11-06 13:48:24 -0500

    updaetd debian/copyright

--- debian/permission-checker/arduinopc.java
+++ debian/permission-checker/arduinopc.java
@@ -1,3 +1,21 @@
+/*
+arduino permission checker prompts the user about required device permissions
+Copyright (C) 2012 Scott Howard <showard@debian.org>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 //javac Example.java
 //jar cfe Example.jar Example *.class
 //java -jar Example.jar
